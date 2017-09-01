9e0122f5366fdd20fd6af3a4e09a570e4811bff4
Alex <aldrake87@users.noreply.github.com>
2017-09-01 15:11:12 +0100

    Update choropleth2.js

--- js/choropleth2.js
+++ js/choropleth2.js
@@ -2,25 +2,25 @@ function choropleth(){
 bubbleChartBool=false
 choroplethBool=true;
 
-d3.tsv("londonSquared.tsv", function(error, data) {
+d3.csv("data/testData.csv", function(error, data) {
 	circles.transition().duration(500)
 		.attr("r", 0);
 	
 	grids.transition().duration(500)
-		.attr("fill", function(d){return colorScale(d.data)})
+		.attr("fill", function(d){return colorScale(d.total)})
 
 		
-	for(i=0; i<burroughGrids.length; i++){
+	for(i=0; i<boroughGrids.length; i++){
 		var ooo;
 		for (j=0; j<data.length; j++){
-			if(data[j].abbreviation==burroughGrids[i].burrough){
+			if(data[j].abbreviation==boroughGrids[i].borough){
 				ooo=j;
 				
 			}
 		}
-		x="#"+burroughGrids[i].burrough;
+		x="#"+boroughGrids[i].borough;
 		d3.select(x).transition().duration(500)
-		.attr("fill", function(d){return colorScale(data[ooo].data)})
+		.attr("fill", function(d){return colorScale(data[ooo].total)})
 		
 		}
 	d3.selectAll(".labels2").transition().duration(500).style("opacity",1);
@@ -34,18 +34,18 @@ choroplethBool=false;
 
 d3.selectAll("#bilbo").select("p").remove();
 
-d3.tsv("londonSquared.tsv", function(error, data) {
+d3.csv("data/testData.csv", function(error, data) {
 	circles.transition().duration(500)
-		.attr("r", function(d){return radiusScale(d.data)})
+		.attr("r", function(d){return radiusScale(d.total)})
 	
 	grids.transition().duration(500)
 		.attr("fill", "#0019A8")
 
 	
 	
-	for(i=0; i<burroughGrids.length; i++){
+	for(i=0; i<boroughGrids.length; i++){
 		
-		x="#"+burroughGrids[i].burrough;
+		x="#"+boroughGrids[i].borough;
 		d3.select(x).transition().duration(500)
 		.attr("fill", "#0019A8")
 		
